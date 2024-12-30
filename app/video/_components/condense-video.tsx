33d7fbb8776169e7f5e53793ee93166dfbc20a8b
Tech-Avrach <nikhilpatel9226@gmail.com>
2024-12-30 21:13:22 +0530

    add video condensing functionality with progress tracking and UI updates

--- app/video/_components/condense-video.tsx
+++ app/video/_components/condense-video.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { AnimatePresence, motion } from 'framer-motion'
 import { CustomeDropZone } from './coustome-dropzone'
 import { acceptedVideoFiles } from '@/utils/formats'
@@ -7,10 +7,16 @@ import VideoDisplay from './video-display'
 import VideoInputDetails from './video-input-details'
 import VideoTrim from './video-trim'
 import VideoInputControl from './video-input-control'
-
+import { FFmpeg } from '@ffmpeg/ffmpeg';
 const condenseVideo = () => {
 
   const [videoFile, setVideoFile] = React.useState<FIleActions>();
+  const [progress, setProgress] = useState<number>(0);
+  const [time, setTime] = useState<{
+    startTime?: number;
+    elapsedSeconds?: number;
+  }>({ elapsedSeconds: 0 });
+  const [status, setStatus] = useState<"notStarted" | "converted" | "condensing">("notStarted");
   const [videoSettings, setVideoSettings] = useState<VideoInputSettings>({
     quality: QualityType.High,
     format: VideoFormats.MP4,
@@ -32,6 +38,10 @@ const condenseVideo = () => {
       isError: false
     })
   }
+
+  const ffmpegRef = useRef(new FFmpeg());
+
+  const disabledDuringCompression = status === "condensing";
 
   return (
     <>
@@ -57,12 +67,28 @@ const condenseVideo = () => {
           {videoFile && <>
             <VideoInputDetails videoFile={videoFile} onClear={() => {}} />
             <VideoTrim 
-            disabled={false} 
+            disabled={disabledDuringCompression} 
             onVideoSettingsChange={setVideoSettings}
             videoSettings={videoSettings}
             />
           </>}
-          <VideoInputControl disable={false} onVideoSettingsChange={setVideoSettings} videoSettings={videoSettings}/>
+        <VideoInputControl disable={disabledDuringCompression} onVideoSettingsChange={setVideoSettings} videoSettings={videoSettings}/>
+
+          <motion.div
+            layout
+            initial={{ scale: 0.9, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            exit={{ scale: 0.9, opacity: 0 }}
+            key={"button"}
+            transition={{type: "tween"}}
+            className='bg-gray-100 border border-gray-200 rounded-2xl p-3 h-fit'
+          >
+            {(status === "notStarted" || status === "converted") && (
+              <button onClick={() => {}} type="button" className='bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-zinc-700 via-zinc-950 to-zinc-950 rounded-lg text-white relative px-3.5 py-2.5 text-sm font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 transition ease-out duration-500 focus:ring-zinc-950 flex-shrink-0'>
+                Condense
+              </button>
+            )}
+          </motion.div>
         </div>
         </motion.div>
       </AnimatePresence>
