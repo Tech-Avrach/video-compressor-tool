312da1d64100ec8cb2409b09c05dd7d3f0eca1e4
Tech-Avrach <nikhilpatel9226@gmail.com>
2025-01-05 22:33:43 +0530

    add video condensing error handling and integrate file conversion utility

--- app/video/_components/condense-video.tsx
+++ app/video/_components/condense-video.tsx
@@ -10,6 +10,7 @@ import VideoInputControl from './video-input-control'
 import { FFmpeg } from '@ffmpeg/ffmpeg';
 import { toBlobURL } from '@ffmpeg/util';
 import { toast } from 'sonner'
+import convertFile from '@/utils/convert'
 const condenseVideo = () => {
 
   const [videoFile, setVideoFile] = React.useState<FIleActions>();
@@ -110,7 +111,23 @@ const condenseVideo = () => {
       ffmpegRef.current.on("log", ({ message }) => {
         console.log(message);
       })
-    }catch {}
+      const { url, output, outputBlob }  = await convertFile(ffmpegRef.current, videoFile, videoSettings);
+      setVideoFile({
+        ...videoFile,
+        url,
+        output,
+        outputBlob
+      });
+      setTime((oldTime) => ({...oldTime, startTime: undefined}));
+      setStatus("converted");
+      setProgress(0);
+    }catch(err) {
+      console.log(err);
+      setStatus("notStarted");
+      setProgress(0);
+      setTime({ elapsedSeconds: 0, startTime: undefined });
+      toast.error("Error condensing video");
+    }
   }
 
 
@@ -156,7 +173,7 @@ const condenseVideo = () => {
           >
             {(status === "notStarted" || status === "converted") && (
               <button 
-              onClick={() => {}} type="button" 
+              onClick={condense} type="button" 
               className='bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-zinc-700 via-zinc-950 to-zinc-950 rounded-lg text-white relative px-3.5 py-2.5 text-sm font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 transition ease-out duration-500 focus:ring-zinc-950 flex-shrink-0'>
                 Condense
               </button>
